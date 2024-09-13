c6e0942627556f9d6f914398e984f3de7a0fcd82
TechPanelGM <giomook2003@gmail.com>
2024-09-13 15:59:50 +0000

    MiuiCamera: Remove commented aux package list

--- miuicamera.mk
+++ miuicamera.mk
@@ -14,10 +14,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera \
     ro.com.google.lens.oem_camera_package=com.android.camera \
-#   vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera \
     ro.miui.notch=1 \
     ro.product.mod_device=umi
-
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
