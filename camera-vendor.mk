6eb05d2b52f3dc3d6507a29bce3ff7238a0a97ce
PabloG <gpablo1@yahoo.com.ar>
2026-07-11 22:30:54 +0200

    Update camera-vendor.mk

--- camera-vendor.mk
+++ camera-vendor.mk
@@ -7,9 +7,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/miuicamera-sm8250/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_algoup_jni.xiaomi.so \
-    vendor/xiaomi/miuicamera-sm8250/proprietary/system/lib64/libcamera_mianode_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_mianode_jni.xiaomi.so \
-    vendor/xiaomi/miuicamera-sm8250/proprietary/system/lib64/libgui-xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libgui-xiaomi.so \
-    vendor/xiaomi/miuicamera-sm8250/proprietary/system/lib64/libmicampostproc_client.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmicampostproc_client.so \
+    vendor/xiaomi/miuicamera-sm8250/proprietary/system/lib64/libcamera_mianode_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_mianode_jni.xiaomi.so 
+
 
 PRODUCT_PACKAGES += \
     MiuiCamera \
