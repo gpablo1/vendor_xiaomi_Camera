70ea5288ccf9ec07780533065fb754cd28593115
PabloG <gpablo1@yahoo.com.ar>
2026-07-13 08:41:11 +0200

    Add new camera libraries to vendor.mk

--- camera-vendor.mk
+++ camera-vendor.mk
@@ -7,4 +7,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_PACKAGES += \
     MiuiCamera \
-    MiuiScanner
+    MiuiScanner \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libgui-xiaomi \
+    libmicampostproc_client \
+    vendor.xiaomi.hardware.campostproc@1.0_system
